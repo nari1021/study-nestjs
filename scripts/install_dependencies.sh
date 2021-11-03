47678a8162cc479cd7f160bd38b9330c297e5b92
nari1021 <y.shhh000@gmail.com>
2021-11-04 08:23:34 +0900

    update :: runas root로 변경

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
-node -e "console.log('Running Node.js ' + process.version)"
-nvm install 14
+sudo nvm install node
+sudo node -e "console.log('Running Node.js ' + process.version)"
+sudo nvm install 14
 
 # Install forever module 
 # https://www.npmjs.com/package/forever
