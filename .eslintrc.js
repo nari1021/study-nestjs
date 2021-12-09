f4d8942768f058f77a23fbf2b0a2fff31afbe021
nari1021 <y.shhh000@gmail.com>
2021-12-09 22:25:59 +0900

    create :: ESLint 설치

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,20 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+  'extends': [
+    'google',
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 13,
+    'sourceType': 'module',
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-      }
-    ]
+  'plugins': [
+    '@typescript-eslint',
+  ],
+  'rules': {
   },
 };
