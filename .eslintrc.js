d4af5854aab4e6bf071567f1ac9c9ca8a0dc2f2f
nari1021 <y.shhh000@gmail.com>
2021-12-10 23:59:55 +0900

    create :: swagger

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,28 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
-    'node': true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaVersion: 13,
+    sourceType: 'module',
   },
-  'extends': [
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended', //프리티어 규칙을 ESLint 규칙으로 추가하는 플러그인
     'google',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier', //프리티어를 사용할 때 ESLint가 코딩스타일에 관해서는 신경안쓰게하려면 eslint-config-prettier를 설치한다. Prettier와 충돌하는 ESLint 규칙을 끄는 역할
+    'prettier',
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 13,
-    'sourceType': 'module',
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
   },
-  'plugins': [
-    '@typescript-eslint',
-  ],
-  'rules': {
+  ignorePatterns: ['.eslintrc.js', '**/migrations/*', '**/seed/*'],
+  rules: {
+    strict: 0,
   },
 };
--- ormconfig.ts
+++ ormconfig.ts
@@ -1,4 +1,4 @@
-import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as path from 'path';
 
 const config: TypeOrmModuleOptions = {
